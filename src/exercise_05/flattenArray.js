25b39f31e465553be380c534b2e2c8ceec8284d2
Chen Sicong <sicong.chen@163.com>
2020-07-25 21:19:06 +0800

    exercise-5

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -6,5 +6,30 @@ export default function flattenArray(array) {
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array == null) {
+    throw new Error('Flatten undefined or null array')
+  }
+
+  let result = [];
+  array.forEach(i => {
+    if (Array.isArray(i)) {
+      i.forEach(t => {
+        result.push(t)
+      });
+    } else {
+      result.push(i)
+    }
+  });
+
+  // function flatten(item) {
+  //   if (Array.isArray(item)) {
+  //     for (let i = 0; i < item.length; i++) {
+  //       flatten(item[i])
+  //     }
+  //   } else {
+  //     result.push(item)
+  //   }
+  // }
+
+  return result
 }
