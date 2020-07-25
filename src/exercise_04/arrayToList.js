384fc461268be628a8a1ae37ac0a9208e553d10b
Chen Sicong <sicong.chen@163.com>
2020-07-25 20:58:34 +0800

    exercise-4

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,30 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  class Node {
+    constructor(value) {
+      this.value = value
+      this.next = null
+    }
+  }
+
+  if (array == null) {
+    throw new Error('Creating list from undefined array')
+  } else if (array.length === 0) {
+    throw new Error('Creating list from empty array')
+  }
+
+  let prevNode = null;
+  let firstNode = null
+  for (let i = 0; i < array.length; i++) {
+    let node = new Node(array[i])
+    if (prevNode != null) {
+      prevNode.next = node;
+    } else {
+      firstNode = node
+    }
+    prevNode = node;
+  }
+
+  return firstNode
 }
