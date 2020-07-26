cb2265382600212f7f8e0cd03cb8b925e60c7ff0
Chen Sicong <sicong.chen@163.com>
2020-07-26 15:12:54 +0800

    exercise-7

--- src/exercise_07/waitForAll.js
+++ src/exercise_07/waitForAll.js
@@ -1,13 +1,44 @@
 export default function waitForAll(...promises) {
-  // This function returns a promise which will be triggered when all the `promises` are completed.
-  //
-  // If all the `promises` are resolved, then the returned promise will be resolved. Otherwise,
-  // if one of the `promises` is rejected, then the returned promise will be rejected.
-  //
-  // Your target:
-  //
-  // * Please implement this function and pass all the tests in wait_for_all_spec.js.
-  // * Please do NOT modify the signature of the function.
+    // This function returns a promise which will be triggered when all the `promises` are completed.
+    //
+    // If all the `promises` are resolved, then the returned promise will be resolved. Otherwise,
+    // if one of the `promises` is rejected, then the returned promise will be rejected.
+    //
+    // Your target:
+    //
+    // * Please implement this function and pass all the tests in wait_for_all_spec.js.
+    // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+    // Promise.allSettled() is unavailable
+
+    promises.forEach(p => {
+        if (!(p instanceof Promise)) {
+            throw new Error('Not all elements are promises.')
+        }
+    })
+
+    // Promise.allSettled() is unavailable in node.js 10.5.3
+    // return new Promise((resolve, reject) => {
+    //     Promise.allSettled(promises).then(results =>{
+    //         results.forEach(result => {
+    //             if (result.status === 'rejected') {
+    //                 reject()
+    //             }
+    //         })
+    //         resolve()
+    //     })
+    // });
+
+    return new Promise((resolve, reject) => {
+        Promise.all(promises.map(p => p.catch(() => false))).then(values => {
+            values.map(v => {
+                if(v === false){
+                    reject()
+                }
+            })
+            resolve()
+        }).catch(() => {
+            reject()
+        })
+    })
 }
